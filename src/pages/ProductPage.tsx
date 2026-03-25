ceab011576bcd24fa93d50f5522ffc10251e57fd
ifubaraboye <ifubaraboyeoribime@outlook.com>
2026-03-25 18:24:41 +0100

    modified file

--- src/pages/ProductPage.tsx
+++ src/pages/ProductPage.tsx
@@ -1,5 +1,5 @@
 import { useState, useRef, useEffect } from "react";
-import { Link, useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { Header } from "@/components/Header";
 import { Footer } from "@/components/Footer";
 import { InfoGrid } from "@/components/InfoGrid";
